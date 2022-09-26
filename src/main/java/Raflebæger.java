ada2b4b2740905a44d41b29c1376281bcae5f914
KEATLLoevkilde <thom4a14@stud.kea.dk>
2022-09-26 13:38:04 +0200

    kommentarer

--- src/main/java/Raflebæger.java
+++ src/main/java/Raflebæger.java
@@ -10,10 +10,12 @@ public class Raflebæger {
         terninger = new ArrayList<Terning>();
     }
     public int ryst(){
+        //Tilføjer terninger
         for (int i = 0; i < antalTerninger; i++) {
             terninger.add(new Terning());
         }
 
+        //Summerer samlede antal øjne
         int samledeAntalØjne = 0;
         for (Terning t: terninger) {
             samledeAntalØjne += t.getØjne();
